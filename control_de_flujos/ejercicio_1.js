d26d90ff3676a4b3356f632327d6639303b55b92
Ezequiel Nicolás Diaz Maceiro <ezequielnicolas@Mac-Ezequiel-Nicolas.local>
2022-09-20 10:23:00 +0200

    ejercicio 2

--- control_de_flujos/ejercicio_1.js
+++ control_de_flujos/ejercicio_1.js
@@ -1,13 +1,17 @@
-let n1 = prompt("Dime un numero");
-let n2 = prompt ("Dime otro numero");
-let n3 = prompt ("Dime el ultimo numero");
+let n1 = prompt("Dime un número");
+let n2 = prompt ("Dime otro número");
+let n3 = prompt ("Dime el ultimo número");
 
-if ( n1 < n2 && n1 < n3) {
-    alert("El número menor es" + n1)
-} 
-else if ( n2 < n1 && n2 < n3) {
-    alert("El número menor es" + n2)
+if ( n1 == n2 && n1 == n3)
+    alert(`Los 3 números son iguales`);
+
+else if ( n1 < n2 && n1 < n3) {
+    alert(`El número menor es ${n1}`);
 }
+
+else if ( n2 < n1 && n2 < n3) 
+    alert(`El número menor es ${n2}`);
+
 else if ( n3 < n1 && n3 < n2) {
-    alert("el número menor es" + n3)
+    alert(`El número menor es ${n3}`);
 }
---
+++ control_de_flujos/ejercicio_2.js
@@ -0,0 +1,16 @@
+let frase = prompt("Dime una frase");
+let letra = prompt("Dime una letra");
+let contador = 0;
+
+for ( let i=0; i<frase.length; i++ ) 
+    if (letra == frase.charAt(i)) {   /*.charAt(i) --> devuelve un caracter que exista en un index especifico de una string.*/
+        contador++;
+    }
+
+if (contador == 0) {
+    alert("Tu letra no está en la frase!")
+}
+
+else if (contador > 0) {
+    alert(`Tu letra aparece ${contador} en la frase`)
+}
